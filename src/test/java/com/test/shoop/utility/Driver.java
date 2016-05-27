6e47c52675e5e3cb8e25e11dd98220b7ba59d8c7
Thadeus Ssewanyana <t.ssewanyana@quidco.com>
2016-05-27 09:20:02 +0100

    fix for fallback match

--- src/test/java/com/test/shoop/utility/Driver.java
+++ src/test/java/com/test/shoop/utility/Driver.java
@@ -6,12 +6,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -23,8 +26,6 @@ public abstract class Driver {
 
 
     public static void driver(String browsertype) throws IOException {
-
-
         if (browsertype.equalsIgnoreCase("firefox") || browsertype.equalsIgnoreCase("")) {
             DesiredCapabilities dc = new DesiredCapabilities();
             driver = new ShoopFireFoxDriver(dc);
@@ -32,18 +33,28 @@ public abstract class Driver {
             if (browsertype.equalsIgnoreCase("chrome")) {
                 DesiredCapabilities dc = DesiredCapabilities.chrome();
                 System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/resources/LinuxbrowserBinaries/chromedriver");
+                       // "/src/test/resources/LinuxbrowserBinaries/chromedriver");
                 driver = new ShoopChromeDriver(dc);
+            }else{
 
+                if (browsertype.equalsIgnoreCase("phantomjs")){
+                    DesiredCapabilities dc = DesiredCapabilities.phantomjs();
+                    dc.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, System.getProperty("user.dir")+"/src/test/resources/LinuxbrowserBinaries/phantomjs");
+                    final List<String> cliArguments = new ArrayList<String>();
+                    cliArguments.add("--web-security=false");
+                    cliArguments.add("--ssl-protocol=any");
+                    cliArguments.add("--ignore-ssl-errors=true");
+                    dc.setCapability("phantomjs.cli.args", cliArguments);
+                    dc.setCapability("takesScreenshot", true);
+                }
             }
-        }
-        {
-            if (browsertype.equalsIgnoreCase("phantomjs")) {
-                DesiredCapabilities dc = DesiredCapabilities.phantomjs();
-                driver = new PhantomJSDriver(dc);
 
-            }
+
         }
+
     }
+
+
 
     public void waitForElementDisplay(final WebElement element) {
 
