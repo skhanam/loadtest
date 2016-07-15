21ac1310eff2fa043834714775d5b11160ffa259
skhanum <s.khanum@quidco.com>
2016-07-15 16:26:12 +0100

    created GotomerchantsPO

--- src/test/java/com/test/shoop/pageobjects/GoTomerchantSitePageObjects.java
+++ src/test/java/com/test/shoop/pageobjects/GoTomerchantSitePageObjects.java
@@ -9,20 +9,6 @@ import org.openqa.selenium.support.FindBy;
  */
 public class GoTomerchantSitePageObjects {
 
-
-    //Search
-    //    public static final String loginLink= ;
-//    public static final String searchHome =;
-//    public static final String SearchResult= ;
-//    public static final String cashBacklink=;
-//    public static final String shoopMerchantPageTittle=;
-//    public static final String wayOutPopup=;
-//    public static final String pageTitle="Groupon Codes Promo, Cashback & Bons de Réduction | Shoop - Le Meilleur du Cashback & du Code Promo";
-//
-
-
-
-
     @FindBy(css = ".form-control.search-input.ng-valid.ng-dirty.ng-valid-parse.ng-touched")
     public WebElement searchHome;
     @FindBy(css = ".e2e-list-merchant-name.ng-binding")
@@ -38,18 +24,5 @@ public class GoTomerchantSitePageObjects {
     @FindBy(xpath = "//a[contains(@href,'je-me-connecte')]")
     public WebElement loginLink;
 
-//    public WebElement searchHome;
-//    @FindBy(css = Constants.searchResult)
-//    public WebElement searchResult;
-//    @FindBy(xpath = Constants.cashBacklink)
-//    public WebElement cashBacklink;
-//    @FindBy(css = ".qp-btn.e2e-obtain-cashback.ng-scope")
-//    public WebElement cashBackAgain;
-//
-//    @FindBy(css = Constants.wayOutPopup)
-//    public WebElement wayOutPopup;
-//
-//    @FindBy(xpath = Constants.loginLink)
-//    public WebElement loginLink;
 
 }
