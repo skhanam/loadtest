36241da40d4c08e670fa05a67fdbefe0b095f8c4
Thadeus12 <t.ssewanyana@quidco.com>
2016-07-19 09:56:18 +0100

    support step def

---
+++ src/test/java/com/test/shoop/page_stepdef/UserCreatesSupportTicketStepDef.java
@@ -0,0 +1,54 @@
+package com.test.shoop.page_stepdef;
+
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+/**
+ * Created by thadeus on 19/07/16.
+ */
+public class UserCreatesSupportTicketStepDef {
+
+    @When("^I click on the aid link in the top right hand corner$")
+    public void i_click_on_the_aid_link_in_the_top_right_hand_corner() throws Throwable {
+
+    }
+
+    @Then("^the user should be transferred with SSO into their own zendesk portal$")
+    public void the_user_should_be_transferred_with_SSO_into_their_own_zendesk_portal() throws Throwable {
+
+    }
+
+    @When("^I selects a support article \"([^\"]*)\"$")
+    public void i_selects_a_support_article(String arg1) throws Throwable {
+
+    }
+
+    @Then("^the user just be directed the this article$")
+    public void the_user_just_be_directed_the_this_article() throws Throwable {
+
+    }
+
+    @When("^I click on \"([^\"]*)\" link$")
+    public void i_click_on_link(String arg1) throws Throwable {
+
+    }
+
+    @When("^I enter inserts the appropriate form with data$")
+    public void i_enter_inserts_the_appropriate_form_with_data() throws Throwable {
+
+    }
+
+    @When("^submit the form$")
+    public void submit_the_form() throws Throwable {
+
+    }
+
+    @Then("^the user should get a confirmation that request has been sent$")
+    public void the_user_should_get_a_confirmation_that_request_has_been_sent() throws Throwable {
+
+    }
+
+
+
+
+}
--- src/test/resources/features/UserCreatesSupportTicket.feature
+++ src/test/resources/features/UserCreatesSupportTicket.feature
@@ -13,14 +13,14 @@ Feature: Users creates a support ticket
     And I click on the login button
 
   Scenario: Go to user zendesk portal
-    When I click on the 'Aide' link in the top right hand corner
+    When I click on the aid link in the top right hand corner
     Then the user should be transferred with SSO into their own zendesk portal
 
   Scenario: directed user to a support article
     When I selects a support article "Qui-valide-mon-cashback"
     Then the user just be directed the this article
 
-  Scenario
+  Scenario:
     When I click on "Contacter le Service Client" link
     And I enter inserts the appropriate form with data
     And submit the form
