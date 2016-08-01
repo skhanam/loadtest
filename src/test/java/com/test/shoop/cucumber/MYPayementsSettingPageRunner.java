0d209e673e51638c764a849d88b49685da83de40
Thadeus12 <t.ssewanyana@quidco.com>
2016-08-01 16:22:23 +0100

    Merge branch 'dev' support and payments page updates
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

---
+++ src/test/java/com/test/shoop/cucumber/MYPayementsSettingPageRunner.java
@@ -0,0 +1,50 @@
+package com.test.shoop.cucumber;
+
+import com.test.shoop.config.AbstractDriver;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+/**
+ * Created by thadeus on 21/07/16.
+ */
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions( tags="@MyPayementsPage",
+        plugin= {"pretty", "html:target/cucumber", "json:target/cucumber.json"},
+        monochrome = true,
+        glue = { "com.test.shoop.page_stepdef",
+                "com.test.shoop.config",
+                "com.test.shoop.page",
+                "com.test.shoop.pageobjects",
+                "com.test.shoop.cucumber"},
+        features={"src/test/resources/features"}
+)
+public class MYPayementsSettingPageRunner {
+
+    private static Logger LOGGER = Logger.getLogger("InfoLogging");
+    @BeforeClass
+    public static void setUp() throws IOException {
+
+        LOGGER.info("Starting testing");
+        AbstractDriver.initialize();
+        AbstractDriver.driver.manage().deleteAllCookies();
+        AbstractDriver.driver.manage().window().maximize();
+
+
+    }
+
+    @AfterClass
+    public  static void tearDown(){
+        LOGGER.info("Quiting browser");
+        AbstractDriver.driver.quit();
+    }
+
+
+}
---
+++ src/test/java/com/test/shoop/pages/MYPayementsSettingsPage.java
@@ -0,0 +1,79 @@
+package com.test.shoop.pages;
+
+import com.test.shoop.config.AbstractDriver;
+import com.test.shoop.pageobjects.MYPayementsSettingsPageObjects;
+import com.test.shoop.pages.HomePage;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
+
+import static org.junit.Assert.assertTrue;
+
+
+/**
+ * Created by thadeus on 21/07/16.
+ */
+public class MYPayementsSettingsPage extends AbstractDriver {
+
+    Actions action = new Actions(driver);
+    HomePage homePage = new HomePage();
+    MYPayementsSettingsPageObjects payment = new MYPayementsSettingsPageObjects();
+
+    public void MYPayementsSettingsPage(){
+        PageFactory.initElements(AbstractDriver.driver, payment);
+    }
+
+    public void goToMyPaymentSettingsPage(String pageLink) {
+        String paymentUrl = homePage.getUrl();
+        String paymentPage = paymentUrl + "/utilisateur/"+"/parametres/" + pageLink;
+        driver.get(paymentPage);
+    }
+
+    public void validateOnUserPaymentPage(){
+        assertTrue(payment.memberPayementsPages.isDisplayed());
+    }
+
+    public void clickOnModifyYourBillingInformationLink(){
+        waitForElementDisplay(payment.modifiedPaymenyLink);
+        payment.modifiedPaymenyLink.click();
+
+    }
+    public void enterCodeBIC(String accout_num){
+        waitForElementDisplay(payment.codeBicField);
+        payment.codeBicField.sendKeys(accout_num);
+
+    }
+    public void enterIBAN(String sort_code){
+        payment.iBANField.sendKeys(sort_code);
+
+    }
+    public void enterTitulaireDuCompte(String n_Name){
+            payment.nameField.sendKeys(n_Name);
+    }
+    public void enterMotDePasseShoop(String n_password){
+        payment.passwordField.sendKeys(n_password);
+
+    }
+    public void clickOnPaymentRecordButton(){
+        payment.paymentRecordButton.click();
+
+    }
+    public void acceptAccountPayementUpdatedTermsAndCond(){
+        waitForElementDisplay(payment.alertAccountAdded);
+        driver.switchTo().alert().accept();
+        payment.alertAccountAdded.click();
+
+    }
+    public void validateMemberPaymentsSuccessfullyUpdated(String updated_accounts){
+        waitForElementDisplay(payment.updateDone);
+        assertTrue(payment.updateDone.getText().contains(updated_accounts));
+    }
+    public void moveToBankingInformationAndClick(){
+        waitForElementDisplay(payment.generalSettings);
+        action.click(payment.generalSettings).
+                moveToElement(payment.bankingInfoLink).
+                click().
+                build().
+                perform();
+    }
+
+}
