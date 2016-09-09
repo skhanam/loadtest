a3e8306164819662ab6a4f3bb51c4b9c5924adbb
Laxmi Maddali <laxmimaddali@Lon-Quid-ML-85.local>
2016-09-09 13:31:14 +0100

    mypayments iban modify link refactored

--- src/test/java/com/test/shoop/pageobjects/MYPayementsSettingsPageObjects.java
+++ src/test/java/com/test/shoop/pageobjects/MYPayementsSettingsPageObjects.java
@@ -7,15 +7,16 @@ import org.openqa.selenium.support.FindBy;
  * Created by thadeus on 21/07/16.
  */
 public class MYPayementsSettingsPageObjects {
-
-
+	
+	
+	
 	@FindBy(css = ".container.ng-binding")
     public WebElement memberPayementsPages;
     @FindBy(xpath = "//*[@id='user.settings.general']")
     public  WebElement generalSettings;
    @FindBy(xpath = "//*[@id='menu-categories']/div[1]/ul/li[3]/ul/li[3]/a")
     public WebElement bankingInfoLink;
-    @FindBy(xpath = "html/body/div[2]/div/ui-view/div/div/div/div[2]/div[2]/div/div[1]/div[3]/a")
+    @FindBy(xpath = "//div[@label='IBAN']/div[3]/a")
     public WebElement modifiedPaymenyLink;
     @FindBy(xpath = "//*[@name = 'bic']")
     public WebElement codeBicField;
